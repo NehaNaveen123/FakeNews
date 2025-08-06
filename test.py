a63283a8543ae209929d2cc8a2dddf18472784c9
Neha Naveen <cookiemonsterneha@gmail.com>
2025-08-06 10:56:32 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,49 @@
+import pdb
+import torch
+import pandas as pd
+fake = pd.read_csv('fakenews/Fake.csv')
+true = pd.read_csv('fakenews/True.csv')
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+class RNN(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = torch.nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = torch.nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+n_hidden = 128
+n_letters = 10
+n_categories = 10
+rnn = RNN(n_letters, n_hidden, n_categories)
+
+input = letterToTensor('A')
+hidden = torch.zeros(1, n_hidden)
+output, next_hidden = rnn(input, hidden)
+pdb.set_trace()
